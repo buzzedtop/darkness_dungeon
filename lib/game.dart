7a8d6da95d287eaf075e9de5f2254e90e6a8d347
Taylor Gleason <taylor@buzzedtop.com>
2025-04-17 22:04:50 -0400

    stack actions

--- lib/game.dart
+++ lib/game.dart
@@ -56,6 +56,7 @@ class GameState extends State<Game> {
         spritePressed: Sprite.load('joystick_atack_range_selected.png'),
         size: 50,
         margin: const EdgeInsets.only(bottom: 140, right: 50),
+        color: Colors.red, // Change the color here
       ),
       JoystickAction(
         actionId: 0,
